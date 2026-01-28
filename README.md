fe35d7ef89c964b93fcd1dcf795fbc275321dccf
Anna C. <alynnechristopherson@gmail.com>
2026-01-28 14:39:42 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 28 14:22:14 2026
+
+@author: anna
+"""
+
---
+++ scr/Event_Plotting/get_data.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 28 14:29:32 2026
+
+@author: anna
+"""
+
