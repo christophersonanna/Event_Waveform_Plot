09c5ca330ad426157c142cc8f89b00ed22c7c9bf
Anna Christopherson <72281415+christophersonanna@users.noreply.github.com>
2026-04-07 15:34:43 -0600

    Delete scr/Event_Plotting/old_scripts directory

--- scr/Event_Plotting/old_scripts/get_data.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import config
-import pandas as pd #[will change later]
-
-#loading data
-parquet_file = pd.read_parquet(config.PROCESSED_DATA_PATH)
